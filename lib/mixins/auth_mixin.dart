af71c768b4080f955f92ba65a7ca21fe4911e2d7
arunxxxl <arunberry47@gmail.com>
2020-10-12 16:06:40 +0530

    facing issue resolving commit

--- lib/mixins/auth_mixin.dart
+++ lib/mixins/auth_mixin.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 mixin AuthMixin {
@@ -32,5 +33,27 @@ mixin AuthMixin {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.clear();
     return;
+  }
+
+  void isLogedin(BuildContext context) async {
+    // SharedPreferences prefs = await SharedPreferences.getInstance();
+    // final String accessToken = prefs.getString("accessToken");
+    // print("$accessToken realy: ${accessToken == null}");
+    // if (accessToken == null) {
+    //   Navigator.of(context).popAndPushNamed("/");
+    // }
+    return;
+  }
+
+  void isNotLogedin(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String accessToken = prefs.getString("accessToken");
+    print(accessToken);
+    if (accessToken == null) {
+      return;
+    } else {
+      Navigator.of(context).pushNamed("/home");
+      return;
+    }
   }
 }
